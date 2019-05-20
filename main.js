5999fd7370c754569941742b518017b93be93013
quinerfor0589 <fain0589@gmail.com>
2019-05-20 18:18:22 -0400

    fix text

--- main.js
+++ main.js
@@ -40,15 +40,14 @@ let answer=document.getElementsByClassName('answer');
 let finalArray=[];
 function answerList(i){
 let answerArray1=["Doctor 58","Medicine 11","Surgeon 11","Biomed. Eng. 3", "Business 3"];//Easy
-let answerArray2=["Spiderman 26","Avengers 11","The Dark Knight 11","Wonderman 11", "Logan 3", "Iron Man 3"];//Medium
+let answerArray2=["Spiderman 26","Avengers 11","The Dark Knight 11","Wonderwoman 11", "Logan 3", "Iron Man 3"];//Medium
 let answerArray3=["John Adams 52","George H.W. Bush 12","James Buchanan 12","Herbert Hoover 12", "Jimmy Carter 10", "Franklin Pierce 8", "James K Polk 4"];//Hard
-let answerArray4=["SIA 17","A&P 14","Chem 11","DOHC 11", "Biology 9", "Medterm 5"]; //Easy
-let answerArray5=["Appendix 29","Gallbladder 23","Spleen 20","Bladder 12", "Kidney/Liver 6"];//Medium
+let answerArray4=["Burpees 37","Push Ups 14","Pulls ups 9","Dead Bugs 6", "Muscle Ups 6"];//Medium
+let answerArray5=["Appendix 29","Gallbladder 23","Spleen 20","Bladder 12", "Kidney 6", "Liver 6"];//Medium
 let answerArray6=["Uganda 74","Uruguay 68","Uzekistan 28","Ukraine 8", "United States 6"];//Hard
 let answerArray7=["Chem. room 29","MMC 17","Senior lounge 14","Cafeteria 11"];//Easy
 let answerArray8=["Lil Uzi Vert 14","Lil Pump 14","Lil Yachty 11","Lil Wayne 6", "Lil Peep 6"];//Medium
 let answerArray9=["Pneumonoultramicroscopicsilicovolcanoconiosis 44","Ankylosing Spondylitis 20","Gastroenterodueodenectomy 12","Sphygmomanometer 12", "Otorhinolaryngologist 12", "Anaphylaxis 6","Myelomeningocele 3"];//Hard
-let answerArray10=["Burpees 37","Push Ups 14","Pulls ups 9","Dead Bugs 6", "Muscle Ups 6"];//Medium
 
 switch(i){
     case 1:
@@ -77,9 +76,6 @@ switch(i){
     break;
     case 9:
         finalArray=answerArray9;
-    break;
-    case 10:
-        finalArray=answerArray10;
     break;
     default:
         finalArray=answerArray1;
